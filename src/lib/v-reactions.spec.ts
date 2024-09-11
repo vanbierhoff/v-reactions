b57748ecc0f234da5ffad11941853f6b59a1a349
hoff <ie@fv.ee>
2024-09-11 09:48:56 +0300

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/setup/connect-workspace/manual?accessToken=undefined&source=create-nx-workspace-success-cache-setup

---
+++ src/lib/v-reactions.spec.ts
@@ -0,0 +1,7 @@
+import { vReactions } from './v-reactions';
+
+describe('vReactions', () => {
+  it('should work', () => {
+    expect(vReactions()).toEqual('v-reactions');
+  });
+});
---
+++ src/lib/v-reactions.ts
@@ -0,0 +1,3 @@
+export function vReactions(): string {
+  return 'v-reactions';
+}
