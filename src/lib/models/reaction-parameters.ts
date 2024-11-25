3a80923590fedf8a76f243be049cae0b9a8efa0d
hoff <ie@fv.ee>
2024-11-25 18:07:08 +0300

    update version

--- src/lib/models/reaction-parameters.ts
+++ src/lib/models/reaction-parameters.ts
@@ -6,9 +6,9 @@ import { BrowserSchedulerStrategyType } from '@v/stack-runner/src/lib/scheduler/
 export const REACTION_RUNNER = (type: BrowserSchedulerStrategyType) => new BaseScheduler(type);
 export const STACK = () => {
   if (typeof REACTION_RUNNER === 'function') {
-    return new Stack(REACTION_RUNNER('sync'));
+    return new Stack(REACTION_RUNNER('sync'), true);
   }
-  return new Stack(REACTION_RUNNER);
+  return new Stack(REACTION_RUNNER, true);
 };
 export const getStack = () => {
   if (typeof STACK === 'function') {
--- src/lib/reactions/reactions.ts
+++ src/lib/reactions/reactions.ts
@@ -58,6 +58,7 @@ export const reactor = <T>(v: T, options?: ReactorOptionsInterface): XReactor<T>
           }
         });
       }
+
     });
   };
 
